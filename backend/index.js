eae6079fe4b532dd1d01478634245a835239dceb
Soumyajeet-Muni <117106268+Soumyajeet-Muni@users.noreply.github.com>
2023-07-08 20:03:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ app.get('/', (req, res) => {
   app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`)
   })
+app.use(cors(corsOptions));
   app.use(express.json())
   app.use('/api/auth', require('./Routes/Auth'));
   
